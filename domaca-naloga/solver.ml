6d7aa3bb1060bc4013c35abc5c8f7ebe1f13664d
Masa Zaucer <masa.zaucer@student.fmf.uni-lj.si>
2022-01-03 08:50:33 +0100

    dodan pregled moznosti za vsako polje

--- domaca-naloga/solver.ml
+++ domaca-naloga/solver.ml
@@ -2,7 +2,7 @@ type available = { loc : int * int; possible : int list }
 
 (* TODO: tip stanja ustrezno popravite, saj boste med reševanjem zaradi učinkovitosti
    želeli imeti še kakšno dodatno informacijo *)
-type state = { problem : Model.problem; current_grid : int option Model.grid }
+type state = { problem : Model.problem; current_grid : int option Model.grid ; available : available option list}
 
 let print_state (state : state) : unit =
   Model.print_grid
@@ -11,8 +11,42 @@ let print_state (state : state) : unit =
 
 type response = Solved of Model.solution | Unsolved of state | Fail of state
 
+let get_elements_of_element element =
+  let list_of_element = Array.to_list element in
+  let rec filter acc l =
+    match l with
+    | [] -> acc
+    | x :: xs -> if Option.is_some x then filter ((Option.get x) :: acc) xs else filter acc xs
+  in filter [] list_of_element
+
+let all_elements_for_element list =
+  let rec aux acc l =
+    match l with
+    | [] -> acc 
+    | x :: xs -> aux (Array.append acc [|get_elements_of_element x|]) xs
+  in aux [||] list
+
+let all_possibilities possibilities_row possibilities_column possibilities_box =
+  let list = List.concat [possibilities_row; possibilities_column; possibilities_box] in
+  let digits = List.init 9 (fun x -> x) in
+  let rec filter acc l =
+    match l with
+    | [] -> acc
+    | x :: xs -> if List.exists ((=) x) list then filter acc xs else filter (x :: acc) xs
+  in filter [] digits
+
+let all_available grid = 
+  let available_rows = all_elements_for_element (Model.rows grid) 
+  and available_columns = all_elements_for_element (Model.columns grid)
+  and available_boxes = all_elements_for_element (Model.boxes grid) in
+  List.flatten (List.init 9 (fun i -> List.init 9 (fun j -> 
+    if Option.is_some grid.(i).(j) then None else 
+      let possible = all_possibilities available_rows.(i) available_columns.(j) available_boxes.(3 * (i / 3) + (j / 3)) in
+      Some {loc=(i, j); possible=possible}
+    )))
+    
 let initialize_state (problem : Model.problem) : state =
-  { current_grid = Model.copy_grid problem.initial_grid; problem }
+  { current_grid = Model.copy_grid problem.initial_grid; problem ; available = all_available problem.initial_grid}
 
 let validate_state (state : state) : response =
   let unsolved =
@@ -31,7 +65,8 @@ let branch_state (state : state) : (state * state) option =
      v prvem predpostavi, da hipoteza velja, v drugem pa ravno obratno.
      Če bo vaš algoritem najprej poizkusil prvo možnost, vam morda pri drugi
      za začetek ni treba zapravljati preveč časa, saj ne bo nujno prišla v poštev. *)
-  failwith "TODO"
+  
+     failwith "TODO"
 
 (* pogledamo, če trenutno stanje vodi do rešitve *)
 let rec solve_state (state : state) =

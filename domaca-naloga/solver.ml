6c93badb5e5764fe98b6fcaf33ce72e0a5c98802
Masa Zaucer <masa.zaucer@student.fmf.uni-lj.si>
2022-01-09 12:56:55 +0100

    dodan osnovni resevalec termometrov

--- domaca-naloga/solver.ml
+++ domaca-naloga/solver.ml
@@ -2,12 +2,16 @@ type available = { loc : int * int; possible : int list }
 
 (* TODO: tip stanja ustrezno popravite, saj boste med reševanjem zaradi učinkovitosti
    želeli imeti še kakšno dodatno informacijo *)
-type state = { problem : Model.problem; current_grid : int option Model.grid; available : available list; just_added : (int * int) option}
+type state = { problem : Model.problem; 
+              current_grid : int option Model.grid; 
+              available : available list; 
+              just_added : (int * int) option;
+              thermometers : (int * int) list list}
 
 let string_of_element (element : available) =
   let (x, y) = element.loc in
   let possible = element.possible in
-  (string_of_int x) ^ "," ^ (string_of_int y) ^ ":" ^ (Model.string_of_list string_of_int "," possible)
+  (string_of_int x) ^ "," ^ (string_of_int y) ^ ":" ^ (Model.string_of_list string_of_int "," possible) (*string_of_tuple*)
 
 let string_of_available available_list =
   Model.string_of_list string_of_element "\n" available_list
@@ -58,9 +62,107 @@ let all_available grid = (* Naredi seznam vseh moznosti po poljih *)
       Some {loc=(i, j); possible=possible}
     )))
     
+
+(*CHECK THERMOMETERS*)
+
+let get_options (i, j) available = (*vrne vse moznosti za mesto (i, j)*)
+  let rec find = function
+    | [] -> 
+      Printf.printf "%s" ("(" ^ string_of_int i ^ "," ^ string_of_int j ^ ")");
+      
+      failwith "Ni moznosti"
+    | option :: rest -> 
+      if option.loc = (i, j) then option.possible else find rest
+  in find available
+
+
+let rec min current_min = function (*vrne minimum int lista*)
+  | [] -> current_min
+  | x :: xs -> 
+    if x < current_min then min x xs else min current_min xs
+
+let rec max current_max = function (*vrne maximum int lista*)
+  | [] -> current_max
+  | x :: xs -> 
+    if x > current_max then max x xs else max current_max xs
+
+let rec check_thermometer acc m direction available grid thermometer = (*direction = true -> minimum | false -> maximum*)
+  match thermometer with
+  | [] ->  acc
+  | (i, j) :: rest -> 
+    if Option.is_some grid.(i).(j) then
+      check_thermometer acc (Option.get grid.(i).(j)) direction available grid rest
+    else
+      let options = get_options (i, j) available in
+      let new_options = List.filter (if direction then ((<) m) else ((>)m)) options in
+      check_thermometer ({loc = (i, j); possible = new_options} :: acc) (if direction then min 9 new_options else max 1 new_options) direction available grid rest
+
+
+let rec switch_el acc available option =
+  match available with
+  | [] -> failwith "Ne obstaja!"
+  | a :: rest ->
+    if a.loc = option.loc then 
+      (option :: rest) @ acc
+    else 
+      switch_el (a :: acc) rest option
+
+
+let rec merge available new_available = 
+  let rec switch available new_available =
+    match new_available with 
+    | [] -> available
+    | x :: xs -> switch (switch_el [] available x) xs
+  in 
+  switch available new_available
+
+let cross_section (list1 : int list) (list2 : int list) : int list =
+  let rec aux acc =function
+  | [] -> acc
+  | x :: xs -> if List.exists (fun y -> x = y) list2 then aux (x :: acc) xs else aux acc xs
+  in aux [] list1
+
+let combine (available' : available list) (available'' : available list) : available list =
+  let rec aux acc = function 
+    | [] -> acc
+    | {loc = (i, j) ; possible} :: rest -> 
+      if List.exists (fun x -> x.loc = (i, j)) available'' then
+        let options = get_options (i, j) available'' in 
+        let new_possible = cross_section possible options in
+        aux ({loc = (i, j) ; possible = new_possible} :: acc) rest
+      else
+        aux ({loc = (i, j) ; possible} :: acc) rest
+  in
+  let rec get_rest acc = function
+    | [] -> acc
+    | option :: rest -> 
+      if List.exists (fun x -> x.loc = option.loc) acc then
+        get_rest acc rest
+      else 
+        get_rest (option :: acc) rest
+  in
+  get_rest (aux [] available') available''
+
+
+let rec check_thermometers (state : state) : state = 
+  let available = state.available in
+  let thermometers = state.thermometers in
+  let available' = List.fold_left combine [] (List.map (check_thermometer [] 0 true available state.current_grid) thermometers) in
+  let available'' = List.fold_left combine [] (List.map (check_thermometer [] 10 false available state.current_grid) (List.map List.rev thermometers)) in
+  let new_available = combine available' available'' in
+  let available = merge available new_available in
+  {state with available = available}
+  
+
+
 let initialize_state (problem : Model.problem) : state =
   let available = List.filter_map (fun x -> x) (all_available problem.initial_grid) in
-  { current_grid = Model.copy_grid problem.initial_grid; problem ; available = available; just_added = None}
+  check_thermometers
+  { current_grid = Model.copy_grid problem.initial_grid; 
+  problem ; 
+  available = available; 
+  just_added = None; 
+  thermometers = problem.thermometers}
 
 let validate_state (state : state) : response =
   let unsolved =
@@ -75,7 +177,10 @@ let validate_state (state : state) : response =
 
 let insert_field (i, j) element (grid : 'a Model.grid) : 'a Model.grid = 
   let map_row x = Array.init 9 (fun y -> if (x, y) = (i, j) then Some element else grid.(x).(y))
-  in Array.init 9 map_row
+  in 
+  Printf.printf "%s" ("(" ^ string_of_int i ^ "," ^ string_of_int j ^ ")");
+  Array.init 9 map_row
+
 
 let branch_state (state : state) : (state * state) option =
   (* TODO: Pripravite funkcijo, ki v trenutnem stanju poišče hipotezo, glede katere
@@ -105,7 +210,8 @@ let rec filter (acc : int list) (el : int) = function
 
 let narrow_options (state : state) : state = (* Izbrise stevke, ki niso vec na razpolago na dolocenem polju *)
   match state.just_added with
-  | None -> state
+  | None -> 
+    check_thermometers state
   | Some (i, j) -> 
     let el = Option.get state.current_grid.(i).(j) in
     let rec correct_related acc l =
@@ -119,15 +225,15 @@ let narrow_options (state : state) : state = (* Izbrise stevke, ki niso vec na r
         else
           correct_related (option :: acc) rest
     in
-    let new_available = correct_related [] state.available in
-    {state with available = new_available; just_added = None}
+    let available' = correct_related [] state.available in
+    check_thermometers {state with available = available'; just_added = None}
 
     
 (* pogledamo, če trenutno stanje vodi do rešitve *)
 let rec solve_state (state : state) =
   (* uveljavimo trenutne omejitve in pogledamo, kam smo prišli *)
   (* TODO: na tej točki je stanje smiselno počistiti in zožiti možne rešitve *)
-  (*print_state state;*)
+  print_state state;
   let new_state = narrow_options state in
   match validate_state new_state with
   | Solved solution ->
